498c2ffd5ea936c7abe5340c4dc93713cd2700c2
lyleverythinG <75658617+lyleverythinG@users.noreply.github.com>
2025-03-05 18:11:22 +0800

    Add profile header view and half of the profile view section

--- FacebookSwiftUI/Model/Profile/View/ProfileView.swift
+++ FacebookSwiftUI/Model/Profile/View/ProfileView.swift
@@ -9,9 +9,38 @@ import SwiftUI
 
 
 struct ProfileView: View {
+    @Environment(\.dismiss) private var dismiss
     
     var body: some View {
-        Text("test")
+        GeometryReader { proxy in
+            ScrollView {
+                VStack {
+                    ProfileHeaderView(width: proxy.size.width)
+                    DividerView(width: proxy.size.width)
+                }
+            }
+            .scrollIndicators(.hidden)
+            .navigationTitle("Juan Dela Cruz")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button(action: {
+                        dismiss()
+                    }, label: {
+                        Image(systemName: "arrow.left")
+                            .foregroundStyle(.black)
+                            .fontWeight(.bold)
+                    })
+                }
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button(action: {}, label: {
+                        Image(systemName: "magnifyingglass")
+                            .foregroundStyle(.black)
+                            .fontWeight(.bold)
+                    })
+                }
+            }
+        }
     }
 }
 
