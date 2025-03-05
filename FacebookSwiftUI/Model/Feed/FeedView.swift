9d471783a79f8a0b86c46012e7f701bc57027eda
lyleverythinG <75658617+lyleverythinG@users.noreply.github.com>
2025-03-05 17:25:02 +0800

    Add Post view reusable

--- FacebookSwiftUI/Model/Feed/FeedView.swift
+++ FacebookSwiftUI/Model/Feed/FeedView.swift
@@ -13,34 +13,40 @@ struct FeedView: View {
     var body: some View {
         NavigationStack {
             GeometryReader { proxy in
-                VStack {
-                    HeaderView()
-                    DividerView(width: proxy.size.width)
-                    StoryFeedView()
-                    DividerView(width: proxy.size.width)
-                    Spacer()
-                }
-                .toolbar {
-                    ToolbarItem(placement: .topBarLeading) {
-                        Text("Facebook")
-                            .font(.system(size: 32, weight: .bold))
-                            .foregroundStyle(facebookBlue)
+                ScrollView {
+                    VStack {
+                        HeaderView()
+                        DividerView(width: proxy.size.width)
+                        StoryFeedView()
+                        DividerView(width: proxy.size.width)
+                        ForEach(0 ..< 3) { _ in
+                            PostView(facebookBlue: facebookBlue)
+                        }
+                        DividerView(width: proxy.size.width - 15)
+                        Spacer()
                     }
-                    ToolbarItem(placement: .topBarTrailing) {
-                        HStack(spacing: 24) {
-                            Image(systemName: "plus.circle.fill")
-                                .resizable()
-                                .scaledToFill()
-                                .frame(width: 24, height: 24)
-                            Image(systemName: "magnifyingglass")
-                                .resizable()
-                                .scaledToFill()
-                                .frame(width: 24, height: 24)
-                                .font(.system(size: 18, weight: .bold))
-                            Image("messenger")
-                                .resizable()
-                                .scaledToFill()
-                                .frame(width: 24, height: 24)
+                    .toolbar {
+                        ToolbarItem(placement: .topBarLeading) {
+                            Text("Facebook")
+                                .font(.system(size: 32, weight: .bold))
+                                .foregroundStyle(facebookBlue)
+                        }
+                        ToolbarItem(placement: .topBarTrailing) {
+                            HStack(spacing: 24) {
+                                Image(systemName: "plus.circle.fill")
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: 24, height: 24)
+                                Image(systemName: "magnifyingglass")
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: 24, height: 24)
+                                    .font(.system(size: 18, weight: .bold))
+                                Image("messenger")
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: 24, height: 24)
+                            }
                         }
                     }
                 }
