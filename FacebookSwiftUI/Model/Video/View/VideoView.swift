4cbf9ce80837d1c1ba66ca62104628decb564459
lyleverythinG <75658617+lyleverythinG@users.noreply.github.com>
2025-03-08 13:31:35 +0800

    Add video view

---
+++ FacebookSwiftUI/Model/Video/View/VideoView.swift
@@ -0,0 +1,35 @@
+//
+//  VideoView.swift
+//  FacebookSwiftUI
+//
+//  Created by Lyle Dane Carcedo on 3/8/25.
+//
+
+import SwiftUI
+
+struct VideoView: View {
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                VideoOptionsView()
+                ForEach(0 ..< 3) { _ in
+                    PostView(isVideo: true)
+                }
+            }
+            .scrollIndicators(.hidden)
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    HStack(spacing: 16) {
+                        Image(systemName: "person.fill")
+                        Image(systemName: "magnifyingglass")
+                    }
+                    .fontWeight(.bold)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    VideoView()
+}
