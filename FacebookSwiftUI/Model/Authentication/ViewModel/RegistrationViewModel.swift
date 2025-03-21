a2a6ccb953282d2a0d99a07fbd755dff8e4d32e0
lyleverythinG <75658617+lyleverythinG@users.noreply.github.com>
2025-03-21 15:06:38 +0800

    Setup firebase

--- FacebookSwiftUI/Model/Authentication/ViewModel/RegistrationViewModel.swift
+++ FacebookSwiftUI/Model/Authentication/ViewModel/RegistrationViewModel.swift
@@ -14,4 +14,14 @@ class RegistrationViewModel: ObservableObject {
     @Published var genderChoices: [String] = ["female", "male", "More options"]
     @Published var age: String = ""
     @Published var email: String = ""
+    @Published var password: String = ""
+    
+    @MainActor
+    func createUser() async throws {
+        if let age = Int(age) {
+            try await AuthService.shared.createUser(email: email, password: password, firstName: firstName, familyName: familyName, age: age, gender: gender)
+        } else {
+            try await AuthService.shared.createUser(email: email, password: password, firstName: firstName, familyName: familyName, age: 0, gender: gender)
+        }
+    }
 }
---
+++ FacebookSwiftUI/Service/AuthService.swift
@@ -0,0 +1,38 @@
+//
+//  AuthService.swift
+//  FacebookSwiftUI
+//
+//  Created by Lyle Dane Carcedo on 3/21/25.
+//
+import FirebaseAuth
+import FirebaseFirestore
+
+
+class AuthService: ObservableObject {
+    
+    static let shared = AuthService()
+    @Published var userSession: FirebaseAuth.User?
+    
+    init () {
+        self.userSession = Auth.auth().currentUser
+    }
+    
+    @MainActor
+    private func uploadUserData(firstName: String,familyName: String, email: String, age:Int,gender: String, id:String) async throws {
+        let user = User(id: id, firstName: firstName, familyName: familyName, email: email, age: age, gender: gender, friendsId: [], friendsRequestsIds: [], isCurrentUser: true) //TODO: Remove isCurrentUser later.
+        
+        guard let userData = try? Firestore.Encoder().encode(user) else { return }
+        try await Firestore.firestore().collection("users").document(id).setData(userData)
+    }
+    
+    @MainActor
+    func createUser(email: String, password: String, firstName: String, familyName: String, age: Int, gender: String) async throws {
+        do {
+            let result = try await Auth.auth().createUser(withEmail: email, password: password)
+            self.userSession = result.user
+            try await uploadUserData(firstName: firstName, familyName: familyName, email: email, age: age, gender: gender, id: result.user.uid)
+        } catch {
+            print("failed to create user \(error.localizedDescription)")
+        }
+    }
+}
